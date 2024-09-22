ccbaea5698e72c0fe30f21ca9427f932b933a658
Freelancer <157552696+eliseuCyberdyni@users.noreply.github.com>
2024-09-22 01:51:28 -0300

    Update javascript.js
    atualizado

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Rota para criar um novo post
+app.post('/posts', (req, res) => {
+    const post = req.body;
+    // Aqui você salvaria o post no banco de dados
+    res.status(201).send(post);
+});
+
+// Rota para listar posts
+app.get('/posts', (req, res) => {
+    // Aqui você buscaria os posts do banco de dados
+    res.send([/* lista de posts */]);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
 
